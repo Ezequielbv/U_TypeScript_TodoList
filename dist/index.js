260e0c3924f6ceac76d250d435a586923946c16d
Ezequiel Benitez <ezequielbv@gmail.com>
2023-01-20 14:37:04 +0100

    functionalities with TypeScript, saving in localstorage

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,42 @@
 "use strict";
+const btn = document.getElementById("btn"); //not null assertion operator (!)
+const input = document.getElementById("todoinput");
+const form = document.querySelector("form");
+const list = document.getElementById("todolist");
+const todos = readTodos();
+todos.forEach(createTodo);
+function readTodos() {
+    const todosJSON = localStorage.getItem("todos");
+    if (todosJSON === null)
+        return [];
+    return JSON.parse(todosJSON);
+}
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+function handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    todos.push(newTodo);
+    createTodo(newTodo);
+    saveTodos();
+    input.value = "";
+}
+function createTodo(todo) {
+    const newLI = document.createElement("LI");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener("change", function () {
+        console.log("clicked");
+        todo.completed = checkbox.checked;
+        saveTodos();
+    });
+    newLI.append(todo.text);
+    newLI.append(checkbox);
+    list.append(newLI);
+}
+form.addEventListener("submit", handleSubmit);
